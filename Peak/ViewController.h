cce42d73ff4e8e694070383ed4fbf37548b5c6a3
Vitaly Berg <bergusman@gmail.com>
2015-02-09 16:54:19 +0300

    Initial Commit

---
+++ Peak/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Peak
+//
+//  Created by Vitaly Berg on 09/02/15.
+//  Copyright (c) 2015 Peak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Peak/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Peak
+//
+//  Created by Vitaly Berg on 09/02/15.
+//  Copyright (c) 2015 Peak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
